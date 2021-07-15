6260324dcfbcdb38dd063050aabd8ff5e2ac70de
Lara Bertram <lara.bertram@study.hs-duesseldorf.de>
2021-07-15 11:32:34 +0200

    Add and edit emotions

--- GYRO/GYRO.ino
+++ GYRO/GYRO.ino
@@ -8,10 +8,10 @@ Adafruit_MPU6050 mpu_right;
 // AD0 to vcc
 Adafruit_MPU6050 mpu_left;
 
-// rgb led pins
+/*// rgb led pins
 int red = 14;
 int green = 15;
-int blue = 12;
+int blue = 12;*/
 
 void setup(void) {
   Wire.begin();
@@ -48,10 +48,10 @@ void setup(void) {
   mpu_left.setFilterBandwidth(MPU6050_BAND_5_HZ);
 
 
-  // initialize rgb led pins
+  /*// initialize rgb led pins
   pinMode(red, OUTPUT);
   pinMode(green, OUTPUT);
-  pinMode(blue, OUTPUT);
+  pinMode(blue, OUTPUT);*/
 }
 
 void loop() {
@@ -64,16 +64,37 @@ void getEmotion() {
   
   if(rightPos == "seitwärts" && leftPos == "seitwärts") {
     Serial.print("Standard-Position");
-    RGB_color(255, 255, 255);
+    //RGB_color(255, 255, 255);
   }
   if(rightPos == "oben" && leftPos == "oben") {
-    Serial.print("Glücklich / Freude");
-    RGB_color(0, 255, 0);
+    Serial.print("Überraschung");
+    //RGB_color(0, 255, 0);
   }
   if(rightPos == "gesicht" && leftPos == "gesicht") {
-    Serial.print("Traurig / Schämen");
-    RGB_color(0, 0, 255);
+    Serial.print("Angst");
+    //RGB_color(0, 0, 255);
   }
+  if((rightPos == "gesicht" && leftPos == "seitwärts") || (rightPos == "seitwärts" && leftPos == "gesicht")) {
+    Serial.print("Enttäuschung / Abneigung");
+  }
+  if((rightPos == "kopf" && leftPos == "seitwärts") || (rightPos == "seitwärts" && leftPos == "kopf")) {
+    Serial.print("Konzentration");
+  }
+  /*
+   * Raus -> Werte zu nah an Standardposition
+   */
+  /*if(rightPos == "brust" && leftPos == "brust") {
+    Serial.print("Wut / Bockigkeit");
+  }*/
+  
+  /*// TODO - Geschwindigkeit
+  if(rightPos == "oben" && leftPos == "oben") {
+    Serial.print("Freude");
+  }
+  // TODO - Geschwindigkeit
+  if() {
+    Serial.print("Wut");
+  }*/
 
   Serial.println("");
   delay(1000);
@@ -94,7 +115,8 @@ String getMPUAccelerationRight() {
   if(a.acceleration.x < 3 && a.acceleration.x > -1) {
     if(a.acceleration.y < -8 && a.acceleration.y > -11) {
       if(a.acceleration.z < 1 && a.acceleration.z > -3) {
-        return "rechts seitwärts";
+        Serial.print("rechts seitwärts");
+        return "seitwärts";
       }
     }
   }
@@ -102,7 +124,8 @@ String getMPUAccelerationRight() {
   if(a.acceleration.x < 11 && a.acceleration.x > 7) {
     if(a.acceleration.y < -1 && a.acceleration.y > -7) {
       if(a.acceleration.z < 6 && a.acceleration.z > 0) {
-        return "rechts oben";
+        Serial.print("rechts oben");
+        return "oben";
       }
     }
   }
@@ -110,7 +133,17 @@ String getMPUAccelerationRight() {
   if(a.acceleration.x < 8 && a.acceleration.x > 4) {
     if(a.acceleration.y < -6 && a.acceleration.y > -10) {
       if(a.acceleration.z < 5 && a.acceleration.z > 1) {
-        return "rechts gesicht";
+        Serial.print("rechts gesicht");
+        return "gesicht";
+      }
+    }
+  }
+
+  if(a.acceleration.x < 10 && a.acceleration.x > 6) {
+    if(a.acceleration.y < 1 && a.acceleration.y > -5) {
+      if(a.acceleration.z < 7 && a.acceleration.z > 3) {
+        Serial.print("rechts kopf");
+        return "kopf";
       }
     }
   }
@@ -131,9 +164,10 @@ String getMPUAccelerationLeft() {
   Serial.println("");
 
   if(a.acceleration.x < 2 && a.acceleration.x > -4) {
-    if(a.acceleration.y < 10 && a.acceleration.y > 6) {
-      if(a.acceleration.z < 0 && a.acceleration.z > -5) {
-        return "links seitwärts";
+    if(a.acceleration.y < 11 && a.acceleration.y > 6) {
+      if(a.acceleration.z < 2 && a.acceleration.z > -5) {
+        Serial.print("links seitwärts");
+        return "seitwärts";
       }
     }
   }
@@ -141,7 +175,8 @@ String getMPUAccelerationLeft() {
   if(a.acceleration.x < 11 && a.acceleration.x > 7) {
     if(a.acceleration.y < 5 && a.acceleration.y > 0) {
       if(a.acceleration.z < 5 && a.acceleration.z > -2) {
-        return "links oben";
+        Serial.print("links oben");
+        return "oben";
       }
     }
   }
@@ -149,16 +184,26 @@ String getMPUAccelerationLeft() {
   if(a.acceleration.x < 10 && a.acceleration.x > 6) {
     if(a.acceleration.y < 8 && a.acceleration.y > 4) {
       if(a.acceleration.z < 5 && a.acceleration.z > -1) {
-        return "links gesicht";
+        Serial.print("links gesicht");
+        return "gesicht";
       }
     }
   }
 
+  /*if(a.acceleration.x < 11 && a.acceleration.x > 8) {
+    if(a.acceleration.y < 3 && a.acceleration.y > -3) {
+      if(a.acceleration.z < 5 && a.acceleration.z > 1) {
+        Serial.print("links kopf");
+        return "kopf";
+      }
+    }
+  }*/
+
   delay(1000);
 }
 
-void RGB_color(int red_value, int green_value, int blue_value) {
+/*void RGB_color(int red_value, int green_value, int blue_value) {
   analogWrite(red, red_value);
   analogWrite(green, green_value);
   analogWrite(blue, blue_value);
-}
+}*/
